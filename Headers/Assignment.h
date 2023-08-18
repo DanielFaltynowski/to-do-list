21061508fe07535cc2abe8b9b5d2e486211a8b75
Daniel <faltynowskidaniel6@gmail.com>
2023-08-18 21:10:36 +0200

    Implementation of the Assignment class.

---
+++ Headers/Assignment.h
@@ -0,0 +1,62 @@
+#ifndef TO_DO_LIST_ASSIGNMENT_H
+#define TO_DO_LIST_ASSIGNMENT_H
+#include <iostream>
+
+using namespace std;
+
+class Assignment
+{
+public:
+    // Constructors
+    Assignment()
+    {
+        label = "Set label...";
+        description = "Set description...";
+        priority = 0;
+    }
+
+    Assignment(string label, string description, unsigned short priority)
+    {
+        this->label = label;
+        this->description = description;
+        this->priority = priority;
+    }
+
+    // Methods
+    string getLabel()
+    {
+        return this->label;
+    }
+
+    string getDescription()
+    {
+        return this->description;
+    }
+
+    unsigned short getPriority()
+    {
+        return this->priority;
+    }
+
+    void setLabel(string label)
+    {
+        this->label = label;
+    }
+
+    void setDescription(string description)
+    {
+        this->description = description;
+    }
+
+    void setPriority(unsigned short priority)
+    {
+        this->priority = priority;
+    }
+
+protected:
+    string label;
+    string description;
+    unsigned short priority;
+};
+
+#endif //TO_DO_LIST_ASSIGNMENT_H
---
+++ Headers/List.h
@@ -0,0 +1,4 @@
+#ifndef TO_DO_LIST_LIST_H
+#define TO_DO_LIST_LIST_H
+
+#endif //TO_DO_LIST_LIST_H
